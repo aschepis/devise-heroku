54a2897fbc3ae8c9fad865937a796220212f6951
Daniel Morrison <daniel@collectiveidea.com>
2013-09-01 09:55:06 -0400

    Use underscore module name in route scope Rails 4 breaks with camel case module name. Still works under Rails 3.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 #DeviseHeroku::Engine.routes.draw do
 Rails.application.routes.draw do
-  scope :module => "DeviseHeroku" do
+  scope :module => "devise_heroku" do
     post "/heroku/sso/login" => "sso#login"
   end
 end
